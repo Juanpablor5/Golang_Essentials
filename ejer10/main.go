85f6d7f786a457ca6758e0837dc73b8112499d1a
Juan Pablo Rocha <jpr.arango10@uniandes.edu.co>
2021-04-29 15:14:05 -0500

    modulos

--- ejer10/main.go
+++ ejer10/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	us "go/ejer10/user"
+	us "golang_essentials/ejer10/user" // go mod init <project_name>
 )
 
 // La estructura de pepe, hereda de usuario
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module go
+module golang_essentials
 
 go 1.16
